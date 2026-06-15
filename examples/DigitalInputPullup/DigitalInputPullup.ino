9c7e4191a4b242d6f8aa113f4fc9354a355d5766
Lucy Amanda Siobhán Anyte Emily Berry <lucyamyphotography@gmail.com>
2026-06-15 13:44:02 -0400

    Add files via upload

---
+++ examples/DigitalInputPullup/DigitalInputPullup.ino
@@ -0,0 +1,49 @@
+/*
+  Input Pull-up Serial
+
+  This example demonstrates the use of pinMode(INPUT_PULLUP). It reads a digital
+  input on pin 2 and prints the results to the Serial Monitor.
+
+  The circuit:
+  - momentary switch attached from pin 2 to ground
+  - LED on pin 13 through a 470+ ohm resistor to ground
+
+  Unlike pinMode(INPUT), there is no pull-down resistor necessary. An internal
+  20K-ohm resistor is pulled to 5V. This configuration causes the input to read
+  HIGH when the switch is open, and LOW when it is closed.
+
+  created 14 Mar 2012
+  by Scott Fitzgerald
+
+  This example code is in the public domain.
+
+  https://docs.arduino.cc/built-in-examples/digital/InputPullupSerial/
+*/
+#include <LapMCP23017.h>
+
+LapMCP23017 io = LapMCP23017(ADDRESS);
+
+void setup() {
+  io.begin();
+  //start serial connection
+  Serial.begin(9600);
+  //configure pin 2 as an input and enable the internal pull-up resistor
+  io.pinMode(2, INPUT_PULLUP);
+  io.pinMode(13, OUTPUT);
+}
+
+void loop() {
+  //read the pushbutton value into a variable
+  int sensorVal = io.digitalRead(2);
+  //print out the value of the pushbutton
+  Serial.println(sensorVal);
+
+  // Keep in mind the pull-up means the pushbutton's logic is inverted. It goes
+  // HIGH when it's open, and LOW when it's pressed. Turn on pin 13 when the
+  // button's pressed, and off when it's not:
+  if (sensorVal == HIGH) {
+    io.digitalWrite(13, LOW);
+  } else {
+    io.digitalWrite(13, HIGH);
+  }
+}
