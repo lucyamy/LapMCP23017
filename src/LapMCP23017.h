4cf262d19fb1e1f47687a666ee4e6d5d6089b40e
Lucy Amanda Siobhán Anyte Emily Berry <lucyamyphotography@gmail.com>
2026-06-15 14:15:06 -0400

    Fix header guard and formatting in LapMCP23017.h

--- src/LapMCP23017.h
+++ src/LapMCP23017.h
@@ -8,10 +8,10 @@
 #define IODIR_B 0x01
 #define IPOL_A 0x02
 #define IPOL_B 0x03
-#define IOCON	0x0A 
+#define IOCON 0x0A 
 #define GPPU_A 0x0C
 #define GPPU_B 0x0D
-#define GPIO_A	0x12
+#define GPIO_A 0x12
 #define GPIO_B 0x13
 
 class LapMCP23017 {
@@ -21,7 +21,7 @@ public:
 	LapMCP23017(uint8_t, TwoWire *);
 	void begin();
 	void pinMode(uint8_t, uint8_t);
-  void portMode(uint8_t, uint8_t, uint8_t);
+    void portMode(uint8_t, uint8_t, uint8_t);
 	uint8_t digitalRead(uint8_t);
 	void digitalWrite(uint8_t, uint8_t);
 	uint8_t read(uint8_t);
