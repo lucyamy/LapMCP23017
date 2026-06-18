cdb2ad4daff8e1e27c7a4cca0cda7bed65df9485
Lucy Amanda Siobhán Anyte Emily Berry <lucyamyphotography@gmail.com>
2026-06-18 18:00:42 -0400

    Refactor LapMCP23017 class with additional constructors
    Add support for up to 8 MCP23017 chips, giving pin numbers from 0 to 127

--- src/LapMCP23017.h
+++ src/LapMCP23017.h
@@ -4,34 +4,42 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-#define IODIR_A 0x00
-#define IODIR_B 0x01
-#define IPOL_A 0x02
-#define IPOL_B 0x03
+#define IODRA 0x00
+#define IODRB 0x01
+#define IPOLA 0x02
+#define IPOLB 0x03
 #define IOCON 0x0A 
-#define GPPU_A 0x0C
-#define GPPU_B 0x0D
-#define GPIO_A 0x12
-#define GPIO_B 0x13
+#define GPPUA 0x0C
+#define GPPUB 0x0D
+#define GPIOA 0x12
+#define GPIOB 0x13
 
 class LapMCP23017 {
 public:
   LapMCP23017();
   LapMCP23017(uint8_t);
+  LapMCP23017(uint8_t, uint8_t);
+  LapMCP23017(uint8_t, uint8_t, uint8_t);
+  LapMCP23017(uint8_t, uint8_t, uint8_t, uint8_t);
+  LapMCP23017(uint8_t, uint8_t, uint8_t, uint8_t, uint8_t);
+  LapMCP23017(uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t);
+  LapMCP23017(uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t);
+  LapMCP23017(uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t);
   LapMCP23017(TwoWire *);
   LapMCP23017(uint8_t, TwoWire *);
   void begin();
   void pinMode(uint8_t, uint8_t);
-  void portMode(uint16_t, uint16_t);
+  void portMode(uint16_t, uint16_t, uint8_t);
   uint8_t digitalRead(uint8_t);
   void digitalWrite(uint8_t, uint8_t);
-  uint16_t read();
-  void write(uint16_t);
+  uint16_t read(uint8_t);
+  void write(uint8_t, uint16_t);
 
 private:
   TwoWire* _wire;
-  uint8_t _addr;
-  uint8_t _read(uint8_t);
-  void _write(uint8_t, uint8_t);
+  uint8_t _addr[8];
+  uint8_t _count;
+  uint8_t _read(uint8_t, uint8_t);
+  void _write(uint8_t, uint8_t, uint8_t);
 };
 #endif
