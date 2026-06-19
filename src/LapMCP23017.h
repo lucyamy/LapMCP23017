a9956bb9cebd49f23481e2d3ecc884f4b0321082
Lucy Amanda Siobhán Anyte Emily Berry <lucyamyphotography@gmail.com>
2026-06-19 12:10:09 -0400

    Add parameter variable names

--- src/LapMCP23017.h
+++ src/LapMCP23017.h
@@ -17,23 +17,23 @@
 class LapMCP23017 {
 public:
   LapMCP23017();
-  LapMCP23017(uint8_t);
-  LapMCP23017(uint8_t, uint8_t);
-  LapMCP23017(uint8_t, uint8_t, uint8_t);
-  LapMCP23017(uint8_t, uint8_t, uint8_t, uint8_t);
-  LapMCP23017(uint8_t, uint8_t, uint8_t, uint8_t, uint8_t);
-  LapMCP23017(uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t);
-  LapMCP23017(uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t);
-  LapMCP23017(uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t);
-  LapMCP23017(TwoWire *);
-  LapMCP23017(uint8_t, TwoWire *);
+  LapMCP23017(uint8_t address);
+  LapMCP23017(uint8_t address1, uint8_t address2);
+  LapMCP23017(uint8_t address1, uint8_t address2, uint8_t address3);
+  LapMCP23017(uint8_t address1, uint8_t address2, uint8_t address3, uint8_t address4);
+  LapMCP23017(uint8_t address1, uint8_t address2, uint8_t address3, uint8_t address4, uint8_t address5);
+  LapMCP23017(uint8_t address1, uint8_t address2, uint8_t address3, uint8_t address4, uint8_t address5, uint8_t address6);
+  LapMCP23017(uint8_t address1, uint8_t address2, uint8_t address3, uint8_t address4, uint8_t address5, uint8_t address6, uint8_t address7);
+  LapMCP23017(uint8_t address1, uint8_t address2, uint8_t address3, uint8_t address4, uint8_t address5, uint8_t address6, uint8_t address7, uint8_t address8);
+  LapMCP23017(TwoWire *wire);
+  LapMCP23017(uint8_t address, TwoWire *wire);
   void begin();
-  void pinMode(uint8_t, uint8_t);
-  void portMode(uint8_t, uint16_t, uint16_t);
-  uint8_t digitalRead(uint8_t);
-  void digitalWrite(uint8_t, uint8_t);
-  uint16_t read(uint8_t);
-  void write(uint8_t, uint16_t);
+  void pinMode(uint8_t pin, uint8_t mode);
+  void portMode(uint8_t chip, uint16_t direction, uint16_t pullup);
+  uint8_t digitalRead(uint8_t pin);
+  void digitalWrite(uint8_t pin, uint8_t value);
+  uint16_t read(uint8_t chip);
+  void write(uint8_t chip, uint16_t value);
 
 private:
   TwoWire* _wire;
